a8b81fc838e932fdfef16d411627e96980406c64
Alla Kaplia <kaplia.alla18@gmail.com>
2023-06-04 19:50:02 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import Expresso from "./Expresso";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Expresso />
+    </>
   );
 };
